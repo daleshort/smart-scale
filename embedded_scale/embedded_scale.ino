1fe6229ebf5134ff01b61542de8c98adeed5aed0
Dale Short <short.dale@gmail.com>
2022-08-12 10:33:21 -0400

    add embedded mqt messaging

--- embedded_scale/embedded_scale.ino
+++ embedded_scale/embedded_scale.ino
@@ -1,9 +1,173 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Basic ESP8266 MQTT example
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
+  It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
+  To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <string.h>
+#include <Arduino.h>
+
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+
+#include "wifi_config.h"
+
+// Update these with values suitable for your network.
+
+AsyncWebServer server(80);
+#define LED 2
+
+// const char* ssid = "CinnamonBuns";
+// const char* password = "hondas2000";
+// const char* mqtt_server = "64.227.29.102";
+
+// #define mqtt_user "mqtt-user"
+// #define mqtt_password "kittens"
+#define generalUpdatePeriod 1000
+unsigned long lastMsgGeneral = 0;
+
+#define TOPIC "scale"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte * payload, unsigned int length) {
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // if ( strcmp(topic, FORCESOILUPDATETOPIC) == 0) {
+  //   updateSoilMoisture();
+  // }
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
+void setup() {
+
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+
+void updateGeneral(void) {
+  float trialNumber = 10.00;
+
+  client.publish(TOPIC, String(trialNumber).c_str());
+  Serial.println( String(trialNumber).c_str());
+
+}
+
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+    updateGeneral();
+    
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recvMsg);
+  server.begin();
+
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsgGeneral > generalUpdatePeriod) {
+    lastMsgGeneral = now;
+    updateGeneral();
+    WebSerial.println("Hello!");
+  }
+
+}
+
+
+void recvMsg(uint8_t *data, size_t len) {
+  WebSerial.println("Received Data...");
+  String d = "";
+  for (int i = 0; i < len; i++) {
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+  if (d == "ON") {
+    digitalWrite(LED, LOW);
+  }
+  if (d == "OFF") {
+    digitalWrite(LED, HIGH);
+  }
 }
---
+++ embedded_scale/wifi_config_example.h
@@ -0,0 +1,6 @@
+const char* ssid = "WifiName";
+const char* password = "wifi_password";
+const char* mqtt_server = "64.xxx.xx.xxx";
+
+const char* mqtt_user = "mqtt-user";
+const char* mqtt_password= "mqttPassword";
